9067c85d01b5f5afc59bd1a0e9c4e911885994da
Amartya Mandal <Amartya.mandal@gmail.com>
2024-05-10 11:46:19 -0400

    updated

--- tests/tfe-pipeline/main.tf
+++ tests/tfe-pipeline/main.tf
@@ -1,21 +1,23 @@
 terraform {
-    required_providers {
-        azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~>2.20.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.20.0"
     }
-    backend "remote" {
-        hostname = "app.terraform.io"
-        organization = "vindpro"
+  }
 
-        workspaces {
-            name = "PoC"
-        }
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "vindpro"
+
+    workspaces {
+      name = "PoC"
     }
+  }
 }
 
 provider "azurerm" {
   features {}
 }
-#cooments
+
+# comments
